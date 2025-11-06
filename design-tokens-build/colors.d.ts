385bf2a0ac02f7254962b806a758ab5c558dea23
Daniel <daniel@tunetek.no>
2025-11-06 17:36:12 +0100

    (chore): added new theme for konferansecenter

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -1,18 +1,11 @@
 /* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
-/* build: v1.6.1 */
+/* build: v1.7.1 */
 import type {} from '@digdir/designsystemet/types';
 
 // Augment types based on theme
 declare module '@digdir/designsystemet/types' {
   export interface ColorDefinitions {
     'primary-color-red': never;
-    'secondary-color-orange': never;
-    'secondary-color-rust': never;
-    'secondary-color-pink': never;
-    'additional-color-ocean': never;
-    'additional-color-jungle': never;
-    brand1: never;
-    brand2: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -1,17 +1,10 @@
-/* build: v1.6.1 */
+/* build: v1.7.1 */
 import type {} from '@digdir/designsystemet/types';
 
 // Augment types based on theme
 declare module '@digdir/designsystemet/types' {
   export interface ColorDefinitions {
     'primary-color-red': never;
-    'secondary-color-orange': never;
-    'secondary-color-rust': never;
-    'secondary-color-pink': never;
-    'additional-color-ocean': never;
-    'additional-color-jungle': never;
-    brand1: never;
-    brand2: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
